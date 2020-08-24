010c7df4060267a8266c9a343adbaef8d8e6ca16
Francis Langlais <francis.langlais70@gmail.com>
2020-08-24 10:37:08 -0400

    Initial commit with project created

---
+++ OgreBattle/Source/OgreBattle/OgreBattle.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OgreBattle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OgreBattle, "OgreBattle" );
